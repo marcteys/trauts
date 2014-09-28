01a33e22cef42abc9e865e07983bce89bceca008
Marc Teyssier <marc.teys@gmail.com>
2014-09-28 21:44:55 +0200

    adding NGUI

--- Unity/trauts/Assets/Scripts/Network/ClientDispatch.cs
+++ Unity/trauts/Assets/Scripts/Network/ClientDispatch.cs
@@ -3,26 +3,19 @@ using System.Collections;
 
 public class ClientDispatch : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Network.isClient && Input.GetMouseButtonDown(1)){
+	void Update ()
+	{
+		if(Network.isClient && Input.GetMouseButtonDown(1))
+		{
 			networkView.RPC ("CreateCircle",RPCMode.All,Vector3.one);
 		}	
 	}
 
 	[RPC]
-	void CreateCircle(Vector3 pos) {
-		
-		 GameObject wavePrefab = (GameObject)Resources.Load ("Prefabs/circle") as GameObject;
-
-			GameObject tmpWave = (GameObject)Instantiate(wavePrefab,pos,wavePrefab.transform.rotation);
-			
-
+	void CreateCircle(Vector3 pos)
+	{
+		GameObject wavePrefab = (GameObject)Resources.Load ("Prefabs/circle") as GameObject;
+		GameObject tmpWave = (GameObject)Instantiate(wavePrefab,pos,wavePrefab.transform.rotation);
 	}
 
 
