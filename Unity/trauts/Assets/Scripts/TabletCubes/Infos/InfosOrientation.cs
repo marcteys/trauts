955be9c98e231ef9e06a145e25e3a73eb09defc9
marcteys <marc.teys@gmail.com>
2014-10-15 16:46:55 +0200

    cleaning info orientation

--- Unity/trauts/Assets/Scripts/TabletCubes/Infos/InfosOrientation.cs
+++ Unity/trauts/Assets/Scripts/TabletCubes/Infos/InfosOrientation.cs
@@ -8,9 +8,6 @@ public class InfosOrientation : MonoBehaviour {
 	private Transform parent;
 	private float speed = 10f;
 
-	private Vector3 targetAngle = Vector3.zero;
-
-
 	private float degree;
 	private float angle;
 
@@ -25,69 +22,34 @@ public class InfosOrientation : MonoBehaviour {
 
 		float gauche = AngleDir(new Vector3(1,0,1),mainCam.position,Vector3.up);
 		float face = AngleDir(new Vector3(-1,0,1),mainCam.position,Vector3.up);
-
-
-		Vector3 targetVector = new Vector3(0,0,0);
-
-
-		Debug.Log ("face " + face + " gauche " + gauche);
 	
 		if(gauche == 1)
 		{
 			if(face == 1)
 			{
-				targetVector =  Vector3.right;
-				targetAngle = new Vector3(0,270,0);
 				degree = 270;
 			}
 			else
 			{
-				targetVector =  -Vector3.forward;
-				targetAngle = new Vector3(0,0,0);
 				degree = 0;
-
 			}
 		}
 		else
 		{
-
 			if(face == 1)
 			{
-				targetVector =  Vector3.forward;
-				targetAngle = new Vector3(0,180,0);
 				degree = 180;
-
 			}
 			else
 			{
-				targetVector =  - Vector3.right;
-				targetAngle = new Vector3(0,90,0);
 				degree = 90;
-
 			}
 		}
 
-
-		Vector3 targetRot = Vector3.Lerp (this.transform.localEulerAngles, targetAngle, Time.deltaTime * speed );
-
-	//	this.transform.localRotation = Quaternion.Euler(targetRot);
-
-		if (Input.GetKeyUp(KeyCode.RightArrow))
-			degree += 90f;
-		if (Input.GetKeyUp(KeyCode.LeftArrow))
-			degree -= 90f;
-		
 		angle = Mathf.LerpAngle(transform.rotation.y, degree, Time.deltaTime);
 		transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, degree,0 ), Time.deltaTime * speed );
 
 	}
-
-
-	void SetNewAngle()
-	{
-	//	targetAngle = 
-	}
-
 
 	float AngleDir (Vector3 fwd, Vector3 targetDir, Vector3 up) {
 		Vector3 perp = Vector3.Cross(fwd, targetDir);
